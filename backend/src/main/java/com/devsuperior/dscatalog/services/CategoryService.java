d1c2903e6e6bd7aaaf9cfa999a7bac726b5b6290
Joao Klug <joaoklug18@gmail.com>
2025-08-11 16:37:17 -0300

    Dto

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.repository.CategoryRepository;
 
 @Service
@@ -14,9 +15,11 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-
-    public List<Category> findAll() {
-        return repository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        
+        //Usando programação funcional para converter category para categoryDto
+        return repository.findAll().stream().map(x -> new CategoryDTO(x)).toList();
     }
 
 }
